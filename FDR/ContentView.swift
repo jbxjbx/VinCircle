f27ef621083c29f86e1f5d040949280136aadfb1
Bob <jbxjbxjbxjbxjbx@hotmail.com>
2026-02-06 14:30:58 -0500

    Initial Commit

---
+++ FDR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FDR
+//
+//  Created by Boxiang Jia on 2/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FDR/FDRApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDRApp.swift
+//  FDR
+//
+//  Created by Boxiang Jia on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct FDRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
